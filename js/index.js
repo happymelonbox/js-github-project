211f108debda87558471379e5d8ca554a31fcd52
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-16 21:37:31 +1000

    working on the display of the username, but I have just noticed that there is a list container to add these items to, so I will need to adjust my syntax so that they are added to the appropriate lsits but otherwise, coming along okay

--- js/index.js
+++ js/index.js
@@ -8,8 +8,20 @@ form.addEventListener('submit', function(event){
     fetch(`https://api.github.com/search/users?q=${input}`)
     .then(response => response.json())
     .then(function(data){
-        console.log(data)
-        
+        let dataKeys = Object.keys(data)
+        let dataValues = Object.values(data)
+        let userNameHeading = document.createElement('h2')
+        userNameHeading.setAttribute('id', 'userNameHeading')
+        let userName = dataValues[2][0].login
+        userNameHeading.innerHTML = 'Username:'
+        document.body.appendChild(userNameHeading)
+        let displayUserName = document.createElement('p')
+        displayUserName.setAttribute('id', 'displayUserName')
+        displayUserName.innerHTML = userName
+        document.body.appendChild(displayUserName)
+
+        // console.log(dataKeys)
+        // console.log(dataValues[2][0].login)
     }
     )
 })
