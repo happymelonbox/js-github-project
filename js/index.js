c506155b49da6d1b732be08840abddfad24eda93
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-16 18:52:51 +1000

    returning the correct user now

--- js/index.js
+++ js/index.js
@@ -2,11 +2,15 @@
 document.addEventListener('DOMContentLoaded', function(){
 let form = document.getElementById('github-form')
 
-
 form.addEventListener('submit', function(event){
     event.preventDefault()
     let input = document.getElementById('search').value
-    console.log(input)
+    fetch(`https://api.github.com/search/users?q=${input}`)
+    .then(response => response.json())
+    .then(function(data){
+        console.log(data)
+        
+    }
+    )
 })
-
 })
