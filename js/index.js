3848534a9cbae65897808707c8be4a43754f2eaf
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-17 12:54:19 +1000

    Lab complete - needs cleaning up but it does as it should

--- js/index.js
+++ js/index.js
@@ -12,16 +12,35 @@ form.addEventListener('submit', function(event){
         let dataValues = Object.values(data)
         let userList = document.getElementById('user-list')
         let userInfo
+        const createNewUserList = function(){
+            let userListUl = userList.appendChild(document.createElement('li')).appendChild(document.createElement('ul'))
+            userListUl.setAttribute('class', 'Users')
+            userListUserName = userListUl.appendChild(document.createElement('li'))
+            userListUserName.innerHTML = `Username: ${userInfo.login}`
+            userListAvatar = userListUl.appendChild(document.createElement('li')).appendChild(document.createElement('img'))
+            userListAvatar.setAttribute('src', `${userInfo.avatar_url}`)
+            userListProfile = userListUl.appendChild(document.createElement('li')).appendChild(document.createElement('a'))
+            userListProfile.innerHTML = 'Profile'
+            userListProfile.setAttribute('target', '_blank')
+            userListProfile.setAttribute('href', `https://github.com/${userInfo.login}`)
+            userListProfile.addEventListener('click', function(){
+                fetch(`https://api.github.com/users/${input}/repos?per_page=100&type=owner`)
+                .then(resp => resp.json())
+                .then((json) => {
+                    console.log(Object(json))
+                    let repoCount = Object(json).length
+                    if (repoCount === undefined){
+                        return repoCount = `0`
+                    }
+                    let repoCountLi = userListUl.appendChild(document.createElement('li'))
+                    repoCountLi.innerHTML = `Total number of Repositories: ${repoCount}`
+                })
+            })
+        }
         for (let i=0;i<dataValues[2].length;i++){
             userInfo = dataValues[2][i]
-            userInnerList.innerHTML = `Username: ${userInfo.login}>`
-            avatarImg.setAttribute('src', `${userInfo.avatar_url}`)
-            profile.innerHTML = 'Profile'
-            profile.setAttribute('target', '_blank')
-            profile.setAttribute('href', `https://github.com/${userInfo.login}`)
-        console.log(dataKeys)
-        console.log(dataValues)
-    }}
-    )
+            createNewUserList(userInfo)
+        }
+        })
 })
 })
