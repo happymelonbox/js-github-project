1727c0da5aa1e3131c99d655fe9cdaf82c8ac84c
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-18 16:54:20 +1000

    fixed indentation to look a bit more tidy and make it easier to read. Added isClicked. Need to add the syntax for if any profile button has been clicked already that it will display tonly the newly clicked user's information to thd. Need to add the syntax to make the application display only the newly clicked users information to the browser when any profile button is clicked.

--- js/index.js
+++ js/index.js
@@ -4,52 +4,59 @@ document.addEventListener('DOMContentLoaded', function(){
     const form = document.getElementById('github-form')
 
     form.addEventListener('submit', function(event){
-    event.preventDefault()
-    let input = document.getElementById('search').value
-    fetch(`https://api.github.com/search/users?q=${input}`)
-    .then(response => response.json())
-    .then(function(data){
-        let dataValues = Object.values(data)
-        let userList = document.getElementById('user-list')
-        let repoList = document.getElementById('repos-list')
-        let userInfo, repoName
-        const createNewUserList = function(){
-            let user = userInfo.login
-            let userListUl = userList.appendChild(document.createElement('li')).appendChild(document.createElement('ul'))
-            userListUl.setAttribute('class', 'Users')
-            userListUserName = userListUl.appendChild(document.createElement('li'))
-            userListUserName.innerHTML = `Username: ${user}`
-            userListAvatar = userListUl.appendChild(document.createElement('li')).appendChild(document.createElement('img'))
-            userListAvatar.setAttribute('src', `${userInfo.avatar_url}`)
-            userListProfile = userListUl.appendChild(document.createElement('li')).appendChild(document.createElement('a'))
-            userListProfile.innerHTML = 'Profile'
-            userListProfile.setAttribute('id', `${user}`)
-            userListProfile.addEventListener('click', function(){
-                fetch(`https://api.github.com/users/${user}/repos?per_page=100&type=owner`,{
-                    cache: 'no-store'
-                })
-                .then(resp => resp.json())
-                .then((json) => {
-                    let repos = Object(json)
-                    if (repos.length === undefined || repos.length === 0){
-                        alert(`No repositories available`)
-                    }
-                    let repoCountLi = userListUl.appendChild(document.createElement('li'))
-                    let repoItems = repoList.appendChild(document.createElement('li'))
-                    repoCountLi.innerHTML = `Total number of Repositories: ${repos.length}`
-                    for (let i=0;i<repos.length;i++){
-                        let repoItem = repoItems.appendChild(document.createElement('li').appendChild(document.createElement('a')))
-                        repoName = repos[i].name
-                        repoItem.innerHTML = `${repoName}`
-                        repoItem.parentElement.setAttribute('class', 'repoItem')
-                        repoItem.setAttribute('class', 'eachRepo')
-                        repoItem.setAttribute('href', `https://www.github.com/${user}/${repoName}`)
-                        repoItem.setAttribute('target', '_blank')
-                    }})
+        event.preventDefault()
+        let input = document.getElementById('search').value
+        fetch(`https://api.github.com/search/users?q=${input}`)
+        .then(response => response.json())
+        .then(function(data){
+            let dataValues = Object.values(data)
+            let userList = document.getElementById('user-list')
+            let repoList = document.getElementById('repos-list')
+            let userInfo, repoName
+            let isClicked = false
+            const createNewUserList = function(){
+                let user = userInfo.login
+                let userListUl = userList.appendChild(document.createElement('li')).appendChild(document.createElement('ul'))
+                userListUl.setAttribute('class', 'Users')
+                userListUserName = userListUl.appendChild(document.createElement('li'))
+                userListUserName.innerHTML = `Username: ${user}`
+                userListAvatar = userListUl.appendChild(document.createElement('li')).appendChild(document.createElement('img'))
+                userListAvatar.setAttribute('src', `${userInfo.avatar_url}`)
+                userListProfile = userListUl.appendChild(document.createElement('li')).appendChild(document.createElement('a'))
+                userListProfile.innerHTML = 'Profile'
+                userListProfile.setAttribute('id', `${user}`)
+                userListProfile.addEventListener('click', function(){
+                    fetch(`https://api.github.com/users/${user}/repos?per_page=100&type=owner`,{
+                        cache: 'no-store'
                     })
-                }
+                    .then(resp => resp.json())
+                    .then((json) => {
+                        let repos = Object(json)
+                        if (!isClicked){
+                            isClicked = true
+                            if (repos.length === undefined || repos.length === 0){
+                                alert(`No repositories available`)
+                            }
+                            let repoCountLi = userListUl.appendChild(document.createElement('li'))
+                            let repoItems = repoList.appendChild(document.createElement('li'))
+                            repoCountLi.innerHTML = `Total number of Repositories: ${repos.length}`
+                            for (let i=0;i<repos.length;i++){
+                                let repoItem = repoItems.appendChild(document.createElement('li').appendChild(document.createElement('a')))
+                                repoName = repos[i].name
+                                repoItem.innerHTML = `${repoName}`
+                                repoItem.parentElement.setAttribute('class', 'repoItem')
+                                repoItem.setAttribute('class', 'eachRepo')
+                                repoItem.setAttribute('href', `https://www.github.com/${user}/${repoName}`)
+                                repoItem.setAttribute('target', '_blank')
+                            }
+                        }
+                    })
+                })
+            }
                 for (let i=0;i<dataValues[2].length;i++){
                     userInfo = dataValues[2][i]
                     createNewUserList(userInfo.login)
                 }
-                })})})
+        })
+    })
+})
