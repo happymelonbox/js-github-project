796958dbd6cb510798b8b0856837af918bcfd6bf
Justin Berger Howes <howes.j.j@gmail.com>
2021-06-21 10:59:52 +1000

    Works as it should, unless a user has no repositories, if a user has no repositories and the profile is clicked it does as it should, but if the next users profile button is clicked, the previous users repo count remains in place until the next user is clicked where the first users repo count is removed but the second users repo count remains... a bug that needs to be ironed out. I also need to clean up my code by creating functions to execute the doubled up code

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const form = document.getElementById('github-form')
-    let toggleButton, input
+    let toggleButton, input, isClicked
+    let usersDisplayed = false
+    let reposDisplayed = false
 
     toggleButton = document.getElementById('toggle')
     toggleButton.addEventListener('click', function(event){
@@ -25,14 +27,21 @@ document.addEventListener('DOMContentLoaded', function(){
             let userList = document.getElementById('user-list')
             let repoList = document.getElementById('repos-list')
             let userInfo, repoName
-            let isClicked = false
+            isClicked = false
+            usersDisplayed = true
             if(input === 0 || input === undefined || input === ''){
                 return alert('Please input a keyword')
+            }
+            if(reposDisplayed){
+                displayedRepos = document.querySelectorAll('li.repoItem')
+                for (i=0;i<displayedRepos.length;i++){
+                    displayedRepos[i].remove()
+                }
             }
             const createNewUserList = function(){
                 let user = userInfo.login
                 let userListUl = userList.appendChild(document.createElement('li')).appendChild(document.createElement('ul'))
-                userListUl.setAttribute('class', 'Users')
+                userListUl.parentNode.setAttribute('class', 'Users')
                 userListUserName = userListUl.appendChild(document.createElement('li'))
                 userListUserName.innerHTML = `Username: ${user}`
                 userListAvatar = userListUl.appendChild(document.createElement('li')).appendChild(document.createElement('img'))
@@ -48,16 +57,30 @@ document.addEventListener('DOMContentLoaded', function(){
                     .then((json) => {
                         let repos = Object(json)
                         let repoCountLi, repoItems, allCurrentRepos, totalRepos
-                        if (isClicked){
-                            allCurrentRepos = document.querySelectorAll('li.repoItem')
-                            allCurrentRepos[0].remove()
-                            totalRepos = document.querySelectorAll('li.totalRepos')
-                            totalRepos[0].remove()
-                        }
                             if (repos.length === undefined || repos.length === 0){
                                 alert(`No repositories available`)
                             }
+                            if (isClicked){
+                                allCurrentRepos = document.querySelectorAll('li.repoItem')
+                                for(i=0;i<allCurrentRepos.length;i++){
+                                    allCurrentRepos[i].remove()
+                                }
+                                totalRepos = document.querySelectorAll('li.totalRepos')
+                                for(j=0;j<allCurrentRepos.length;j++){
+                                    totalRepos[j].remove()
+                                }
+                            }
                             isClicked = true
+                            if (isClicked){
+                            allCurrentRepos = document.querySelectorAll('li.repoItem')
+                            for(i=0;i<allCurrentRepos.length;i++){
+                                allCurrentRepos[i].remove()
+                            }
+                            totalRepos = document.querySelectorAll('li.totalRepos')
+                            for(j=0;j<allCurrentRepos.length;j++){
+                                totalRepos[j].remove()
+                            }
+                        }
                             repoCountLi = userListUl.appendChild(document.createElement('li'))
                             repoItems = repoList.appendChild(document.createElement('li'))
                             repoCountLi.setAttribute('class', 'totalRepos')
@@ -79,12 +102,51 @@ document.addEventListener('DOMContentLoaded', function(){
                     createNewUserList(userInfo.login)
                 }
         })
-    }else if (toggleButton.className === 'toggleToUsers'){
-        fetch(`https://api.github.com/search/repositories?q=${input} in:name`, {
-        headers: {
-                'Accept': 'application/vnd.github.v3.text-match+json'}})
-        .then(resp => resp.json())
-        .then((data) => {
-            console.log(data)
-        })}
-})})
+            }else if (toggleButton.className === 'toggleToUsers'){
+                fetch(`https://api.github.com/search/repositories?q=${input} in:name&per_page=100`, {
+                    headers: {
+                        'Accept': 'application/vnd.github.v3.text-match+json'}})
+                .then(resp => resp.json())
+                .then((data) => {
+                    let foundRepos = Object.values(data)[2]
+                    reposDisplayed = true
+                    let repoList = document.getElementById('repos-list')
+                    let repoUsername, repoName, repoUrl, eachFoundRepo, repoCountLi
+                    if(usersDisplayed){
+                        displayedUsers = document.querySelectorAll('li.users')
+                        for (i=0;i<displayedUsers.length;i++){
+                            displayedUsers[i].remove()
+                        }
+                    }
+                    if (isClicked || reposDisplayed){
+                        allCurrentRepos = document.querySelectorAll('li.repoItem')
+                        for(i=0;i<allCurrentRepos.length;i++){
+                            allCurrentRepos[i].remove()
+                        }
+                    }
+                    // if(reposDisplayed){
+                    //     displayedRepos = document.querySelectorAll('li.repoItem')
+                    //     for (i=0;i<displayedRepos.length;i++){
+                    //         displayedRepos[i].remove()
+                    //     }
+                    // }
+                    repoCountLi = repoList.appendChild(document.createElement('li'))
+                    repoItems = repoList.appendChild(document.createElement('li'))
+                    repoCountLi.setAttribute('class', 'totalRepos repoItem')
+                    repoCountLi.innerHTML = `Total number of Repositories: ${foundRepos.length}`+'<br/><br/>'
+                    for(j=0;j<foundRepos.length;j++){
+                        eachFoundRepo = foundRepos[j]
+                        repoUsername = eachFoundRepo.owner.login
+                        repoName = eachFoundRepo.name
+                        repoUrl = eachFoundRepo.html_url
+                        let repoItem = repoItems.appendChild(document.createElement('li').appendChild(document.createElement('a')))
+                        repoItem.innerHTML = '<strong>Repository name: </strong>'+repoName+'<br/>'+'<strong>Repository owner: </strong>'+repoUsername+'<br/>'+'<strong>Repository Link: </strong>'+repoUrl+'<br/><br/>'
+                        repoItem.parentElement.setAttribute('class', 'repoItem')
+                        repoItem.setAttribute('class', 'eachRepo')
+                        repoItem.setAttribute('href', `${repoUrl}`)
+                        repoItem.setAttribute('target', '_blank')
+                    }
+                })
+            }
+    })
+})
